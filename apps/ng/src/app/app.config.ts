88f144f5374e1f3767336f06858ca0cbdc296670
Reonido <faiteforever@gmail.com>
2024-05-03 20:50:59 +0300

    - Set theme - Installed NgRx

--- apps/ng/src/app/app.config.ts
+++ apps/ng/src/app/app.config.ts
@@ -3,12 +3,22 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideStore } from '@ngrx/store';
+
 import { appRoutes } from './app.routes';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withComponentInputBinding()),
     provideHttpClient(withFetch()),
     provideAnimations(),
-  ],
+
+    provideStore(),
+    provideRouterStore(),
+    provideStoreDevtools(),
+    provideEffects([])
+  ]
 };
---
+++ dl-packages/README.md
@@ -0,0 +1,7 @@
+# dl-packages
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test dl-packages` to execute the unit tests.
