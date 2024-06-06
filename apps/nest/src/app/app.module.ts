5f282b2411c17ae90d859b82171c15fc8e930c0f
Reonido <faiteforever@gmail.com>
2024-06-06 22:03:05 +0300

    Updated DB config

--- apps/nest/src/app/app.module.ts
+++ apps/nest/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 import { UserModule } from './features/user/user.module';
@@ -8,18 +8,27 @@ import { AuthModule } from './features/auth/auth.module';
 import { MapModule } from './features/map/map.module';
 import { AppController } from './app.controller';
 
+const devDbConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  password: process.env.POSTGRES_PASSWORD,
+  username: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DATABASE,
+  port: Number(process.env.POSTGRES_PORT),
+  autoLoadEntities: true,
+  synchronize: true
+};
+
+const prodDbConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  url: process.env.DB_URL,
+  autoLoadEntities: true,
+  synchronize: true
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env', }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      password: process.env.POSTGRES_PASSWORD,
-      username: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DATABASE,
-      port: Number(process.env.POSTGRES_PORT),
-      autoLoadEntities: true,
-      synchronize: true
-    }),
+    TypeOrmModule.forRoot(process.env.ENV === 'dev' ? devDbConfig : prodDbConfig),
     UserModule,
     AuthModule,
     MapModule
